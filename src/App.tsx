2687bd4b588cb11a7306e0992afa5b8a6733618c
nordstromkristofer <nordstromkristofer@gmail.com>
2025-02-19 20:33:26 +0100

    calendar folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Calendar from "./Calendar"
+import Calendar from "./calendar/Calendar"
 import Home from "./Home"
 
 import './App.css';
