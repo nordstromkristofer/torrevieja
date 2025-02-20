8b74f4ca53d4d0f1d731de55488a5eae9a5a612e
nordstromkristofer <nordstromkristofer@gmail.com>
2025-02-20 16:47:47 +0100

    picborderradius

--- src/slideshow.tsx
+++ src/slideshow.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Autoplay } from "swiper/modules";
-import pic from "./pics/cl_sign_fasad_01_2015.webp"
+import pic from "./pics/IMG_9918.jpg"
 import pic2 from "./pics/IMG_8240.jpg"
+import pic3 from "./pics/IMG_9922.jpg"
 import "./slideshow.css"
 import "swiper/css";
 import "swiper/css/navigation";
@@ -10,6 +11,7 @@ import "swiper/css/navigation";
 const images = [
   pic,
   pic2,
+  pic3
 ];
 
 const Slideshow: React.FC = () => {
